2940a9fecb336a98cb93266227a629d2dec3fa18
rcatlord <henrypartridge@henrys-mbp.tht.local>
2019-11-14 10:56:22 +0000

    Allotments

---
+++ assets/allotments/README.md
@@ -0,0 +1,34 @@
+<table>
+<tr>
+	<td>Dataset</td>
+	<td>Allotments</td>
+</tr>
+<tr>
+	<td>Description</td>
+	<td>Allotments in Trafford</td>
+</tr>
+<tr>
+	<td>Frequency</td>
+	<td>NA</td>
+</tr>
+<tr>
+	<td>Licence</td>
+	<td><a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence 3.0</a> ; <a href="https://www.openstreetmap.org/copyright">Open Data Commons Open Database License (ODbL)</a></td>
+</tr>
+<tr>
+	<td>Format</td>
+	<td>GeoJSON, CSV</td>
+</tr>
+<tr>
+	<td>Openness rating</td>
+	<td>&#9733&#9733&#9733&#9734&#9734&nbsp; Structured data in a non-proprietary format (e.g. CSV)</td>
+</tr>
+<tr>
+	<td>Last updated</td>
+	<td>2019-11-13</td>
+</tr>
+<tr>
+	<td>Page on data.gov.uk</td>
+	<td><a href="#">View</a></td>
+</tr>
+</table>
---
+++ assets/allotments/pre-processing.R
@@ -0,0 +1,49 @@
+# Allotments #
+
+# Source: Trafford Council and OpenStreetMap
+# Publisher URL: https://www.trafford.gov.uk/residents/leisure-and-lifestyle/parks-and-open-spaces/allotments
+# Licence: OGL 3.0 ; © OpenStreetMap contributors (ODbL)
+
+# load libraries ---------------------------
+library(tidyverse) ; library(rvest) ; library(osmdata) ; library(sf)
+
+# scrape data ---------------------------
+html <- read_html("https://www.trafford.gov.uk/residents/leisure-and-lifestyle/parks-and-open-spaces/allotments/allotments-in-trafford.aspx")
+df <- tibble(name = html_text(html_nodes(html, ".sys_t2643"))) %>% 
+  mutate(name = str_replace_all(name, "allotments|Allotments|Allotment|Allotment Site", ""),
+         name = str_replace(name, "Chadwick Road", "Chadwick Park"),
+         name = str_replace(name, "Grove Lane", "Quarry Bank"),
+         name = str_replace(name, "Lesley Road / Moss Park", "Lesley Road/Moss Park"),
+         name = str_replace(name, "Moss Lane / Golf Road", "Moss Lane/Golf Road"),
+         name = str_replace(name, "Trafford Drive / Beech Avenue", "Trafford Drive/Beech Avenue"),
+         name = str_replace(name, "Woodstock", "Woodstock Road"),
+         name = str_replace(name, "De Quincey Road", "Woodcote Road"),
+         name = str_replace(name, "Tavistock", "Tavistock Road"),
+         name = str_replace(name, "Seymour Grove", "Old Trafford"),
+         name = str_replace(name, "Moss view", "Moss View Road"),
+         name = str_replace(name, "Malborough", "Marlborough Road"),
+         name = str_trim(name))
+
+# retrieve allotments from OpenStreetMap ---------------------------
+osm <- opq(bbox = c(-2.478454,53.35742,-2.253022,53.48037)) %>%
+  add_osm_feature(key = "landuse", value = "allotments") %>%
+  osmdata_sf()
+
+# prepare for matching ---------------------------
+osm <- osm$osm_polygons %>%
+  mutate(name = str_replace(name, "Moss View Road Allotment Site", "Moss View Road"),
+         name = str_replace_all(name, "allotments|Allotments|Allotment", ""),
+         name = str_replace(name, "Lesley Road / Moss Park", "Lesley Road/Moss Park"),
+         name = str_replace(name, "Trafford Drive/Beech Ave", "Trafford Drive/Beech Avenue"),
+         name = str_replace(name, "Trafford Drive / Beech Ave", "Trafford Drive / Beech Avenue"),
+         name = str_replace(name, "Wellfield", "Wellfield Lane"),
+         name = str_trim(name)) %>% 
+  select(name, osm_id)
+
+# join OpenStreetMap geometries ---------------------------
+sf <- df %>% 
+  left_join(osm, by = "name") %>% 
+  st_as_sf(crs = 4326)
+
+# write data ---------------------------
+st_write(sf, "allotments.geojson")
