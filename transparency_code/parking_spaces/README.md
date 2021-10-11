9f8d98e00e4a9b1f544456c78aa522bfd9fee3f8
James Austin <james.austin@trafford.gov.uk>
2021-10-11 12:38:22 +0100

    Parking spaces data.

---
+++ transparency_code/parking_spaces/README.md
@@ -0,0 +1,36 @@
+## Parking Spaces
+
+<table>
+<tr>
+	<td>Dataset</td>
+	<td>Parking Spaces</td>
+</tr>
+<tr>
+	<td>Description</td>
+	<td>Information relating to the off-street and on-street parking spaces controlled by Trafford Council.</td>
+</tr>
+<tr>
+	<td>Frequency</td>
+	<td>Annual</td>
+</tr>
+<tr>
+	<td>Licence</td>
+	<td><a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">OGL v3.0</a></td>
+</tr>
+<tr>
+	<td>Format</td>
+	<td>CSV</td>
+</tr>
+<tr>
+	<td>Openness rating</td>
+	<td>&#9733;&#9733;&#9733;&#9734;&#9734;&nbsp; Structured data in a non-proprietary format (e.g. CSV)</td>
+</tr>
+<tr>
+	<td>Last updated</td>
+	<td>2021-09-20</td>
+</tr>
+<tr>
+	<td>Page on data.gov.uk</td>
+	<td></td>
+</tr>
+</table>
---
+++ transparency_code/parking_spaces/pre-processing.R
@@ -0,0 +1,19 @@
+## Transparency Code - Parking Spaces ##
+# Source: Trafford Council
+# Publisher URL: https://www.trafford.gov.uk/about-your-council/data-protection/open-data/docs/trafford-off-street-parking-spaces.csv
+#                https://www.trafford.gov.uk/about-your-council/data-protection/open-data/docs/trafford-on-street-parking-spaces.csv
+# Licence: Open Government Licence v3.0
+
+# Load the necessary R packages
+library(tidyverse)
+
+# Read in the data to separate tibbles
+df_off_street_raw <- read_csv("https://www.trafford.gov.uk/about-your-council/data-protection/open-data/docs/trafford-off-street-parking-spaces.csv")
+df_on_street_raw <- read_csv("https://www.trafford.gov.uk/about-your-council/data-protection/open-data/docs/trafford-on-street-parking-spaces.csv")
+
+# Tidy up the data
+# - nothing required for these datasets
+
+# Write out the cleaned data
+write_csv(df_off_street_raw, "off_street_parking_spaces.csv")
+write_csv(df_on_street_raw, "on_street_parking_spaces.csv")
