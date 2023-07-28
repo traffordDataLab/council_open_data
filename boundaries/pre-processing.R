4f8026643cd4fa93ec1c8932ab3e7728a36de64f
James Austin <james.austin@trafford.gov.uk>
2023-07-28 18:40:52 +0100

    Updated with the latest boundaries available from ONS Geography Portal. This includes the new wards and localities for Trafford which came into effect on 2023-05-04.

--- boundaries/pre-processing.R
+++ boundaries/pre-processing.R
@@ -1,11 +1,11 @@
 # Administrative, electoral and statistical boundaries for Trafford #
-# December 2021, generalised resolution (20m)
+# Latest versions of generalised resolution (20m) as at 2023-07-28
 
 # NOTES:
 # These boundaries are obtained from ONS Open Geography Portal using the API service: https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/{BOUNDARY TYPE}/FeatureServer/0/query?{API PARAMETERS}
-# The default API call shown on each dataset page returns features for the whole county, therefore it's best to use some extra parameters to reduce the amount of data being returned and speed up the process.
+# The default API call shown on each dataset page returns features for the whole county, therefore it's best to use some extra parameters, such as defining a rectangle (spatial envelope) around the LA, to reduce the amount of data being returned and speed up the process.
 # Local Authority (LA) boundaries can be obtained using the area code or name, e.g. E08000009 or "Trafford".
-# For boundaries within an LA that don't contain the LA's area code or name you can define a rectangle (spatial envelope) around the LA and then use the st_intersection() function to obtain the features that intersect with the LA's boundary, or use a lookup.
+# For boundaries within an LA that don't contain the LA's area code or name you can use the st_intersection() function to obtain the features that intersect with the LA's boundary, or use a lookup.
 # To get the spatial rectangle coordinates around the area you require, and the other API properties, simply zoom into the interactive map shown on the ONS Geoportal page for the boundary data you require, then select "I want to use this" and expand the "View API resources" box.
 # All methods described above are demonstrated within this script.
 
@@ -20,21 +20,21 @@ api_geommetry_envelope <- "&geometryType=esriGeometryEnvelope&geometry=%7B%22spa
 
 # Local authority district -------------------------
 # Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/ons::local-authority-districts-december-2021-gb-bgc/about
+# URL: https://geoportal.statistics.gov.uk/datasets/ons::local-authority-districts-may-2023-boundaries-uk-bgc/about
 # Licence: OGL v3.0
 # NOTE: we need the LA boundary stored as an sf object for use in st_intersection() calculations for other boundaries
-la <- st_read("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_December_2021_GB_BGC/FeatureServer/0/query?outFields=*&where=UPPER(lad21cd)%20like%20%27%25E08000009%25%27&f=geojson") %>% 
-  select(area_code = LAD21CD, area_name = LAD21NM) %>%
+la <- st_read("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Local_Authority_Districts_May_2023_UK_BGC_V2/FeatureServer/0/query?outFields=*&where=UPPER(lad23cd)%20like%20%27%25E08000009%25%27&f=geojson") %>%
+  select(area_code = LAD23CD, area_name = LAD23NM) %>%
   st_write("local_authority_district.geojson")
 
 
 # Westminster Parliamentary Constituencies -------------------------
 # Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/ons::westminster-parliamentary-constituencies-december-2021-uk-bgc/about
+# URL: https://geoportal.statistics.gov.uk/datasets/ons::westminster-parliamentary-constituencies-december-2022-uk-bgc/about
 # Licence: OGL v3.0
-st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Westminster_Parliamentary_Constituencies_DEC_2021_UK_BGC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson", api_geommetry_envelope)) %>% 
+st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Westminster_Parliamentary_Constituencies_Dec_2022_UK_BGC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson", api_geommetry_envelope)) %>% 
   st_intersection(la) %>% 
-  select(area_code = PCON21CD, area_name = PCON21NM) %>% 
+  select(area_code = PCON22CD, area_name = PCON22NM) %>% 
   # we need to filter further as some features returned are not applicable
   filter(area_name %in% c("Altrincham and Sale West", "Stretford and Urmston", "Wythenshawe and Sale East")) %>% 
   st_write("westminster_parliamentary_constituency.geojson")
@@ -42,26 +42,26 @@ st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/servic
 
 # Electoral wards -------------------------
 # Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/ons::wards-december-2021-gb-bgc/about
+# URL: https://geoportal.statistics.gov.uk/datasets/ons::wards-may-2023-boundaries-uk-bgc/about
 # Licence: OGL v3.0
-wards <- st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Wards_December_2021_GB_BGC/FeatureServer/0/query?&outFields=*&where=1%3D1&f=geojson", api_geommetry_envelope)) %>% 
-  st_intersection(la) %>%
-  select(area_code = WD21CD, area_name = WD21NM) %>% 
+wards <- st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/WD_MAY_2023_UK_BGC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson", api_geommetry_envelope)) %>% 
+  filter(LAD23NM == "Trafford") %>%
+  select(area_code = WD23CD, area_name = WD23NM) %>%
   st_write("electoral_ward.geojson")
 
 
 # Localities -------------------------
 # These are a Trafford council-defined construct based on groupings of electoral wards
 # Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/ons::wards-december-2021-gb-bgc/about
+# URL: https://geoportal.statistics.gov.uk/datasets/ons::wards-may-2023-boundaries-uk-bgc/about
 # Licence: OGL v3.0
 localities <- wards %>%
   mutate(area_name = 
            case_when(
-             area_name %in% c("Ashton upon Mersey", "Brooklands", "Priory", "St Mary\'s", "Sale Moor") ~ "Central",
-             area_name %in% c("Clifford", "Gorse Hill", "Longford", "Stretford") ~ "North",
-             area_name %in% c("Altrincham", "Bowdon", "Broadheath", "Hale Barns", "Hale Central", "Timperley", "Village") ~ "South",
-             area_name %in% c("Bucklow-St Martins", "Davyhulme East", "Davyhulme West", "Flixton", "Urmston") ~ "West")) %>% 
+               area_name %in% c("Ashton upon Mersey", "Brooklands", "Manor", "Sale Central", "Sale Moor") ~ "Central",
+               area_name %in% c("Gorse Hill & Cornbrook", "Longford", "Lostock & Barton", "Old Trafford", "Stretford & Humphrey Park") ~ "North",
+               area_name %in% c("Altrincham", "Bowdon", "Broadheath", "Hale", "Hale Barns & Timperley South", "Timperley Central", "Timperley North") ~ "South",
+               area_name %in% c("Bucklow-St Martins", "Davyhulme", "Flixton", "Urmston") ~ "West")) %>% 
   group_by(area_name) %>% 
   summarise() %>% 
   st_write("localities.geojson")
@@ -69,11 +69,11 @@ localities <- wards %>%
 
 # Parishes -------------------------
 # Source: ONS Open Geography Portal
-# URL: https://geoportal.statistics.gov.uk/datasets/ons::parishes-december-2021-ew-bgc/about
+# URL: https://geoportal.statistics.gov.uk/datasets/ons::parishes-may-2023-boundaries-ew-bgc-2/about
 # Licence: OGL v3.0
-st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Parishes_December_2021_EW_BGC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson", api_geommetry_envelope)) %>% 
+st_read(paste0("https://services1.arcgis.com/ESMARspQHYMw9BZ9/arcgis/rest/services/Parishes_May_2023_Boundaries_EW_BGC/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson", api_geommetry_envelope)) %>% 
   st_intersection(la) %>%
-  select(area_code = PAR21CD, area_name = PAR21NM) %>%
+  select(area_code = PAR23CD, area_name = PAR23NM) %>%
   # we need to filter further as some features returned are not applicable
   filter(area_name %in% c("Carrington", "Dunham Massey", "Partington", "Warburton")) %>% 
   st_write("parish.geojson")
